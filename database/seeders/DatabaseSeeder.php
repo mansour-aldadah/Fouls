d2948156a21daa81f3841bdb61d30a28b7b31b32
Mansour  Al-Dadah <mansouraldadah@gmail.com>
2024-07-11 14:33:49 +0300

    v 2.1 add default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use function Laravel\Prompts\password;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'username' => 'adminadmin',
+            'password' => Hash::make('admin'),
+            'role' => 'مدير'
+        ]);
     }
 }
