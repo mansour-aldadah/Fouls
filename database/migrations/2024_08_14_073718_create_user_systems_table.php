045094a886449710eac570cf838ba1c4a3034661
Mansour  Al-Dadah <mansouraldadah@gmail.com>
2024-08-14 10:40:19 +0300

    v3.5(create system table , user_systems_table)

---
+++ database/migrations/2024_08_14_073718_create_user_systems_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_systems', function (Blueprint $table) {
+            $table->id();
+            $table->foreignid('user_id')->constrained()->restrictOnDelete();
+            $table->foreignid('system_id')->constrained()->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_systems');
+    }
+};
